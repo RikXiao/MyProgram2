2a7dde7d87f5e1b7263671196be5d6bd93e53b30
RikXiao <274146571@qq.com>
2017-08-29 21:35:04 +0800

    三个节点的最小根节点+trycathch嵌套

--- MyProgram/src/NiuKe/son.java
+++ MyProgram/src/NiuKe/son.java
@@ -1,6 +1,9 @@
 package NiuKe;
 
 public class son extends patrent {
+	public son(){
+		System.out.println(1);
+	}
 public static void main(String[] args) {
 	son son1 = new son();
 	System.out.println();
